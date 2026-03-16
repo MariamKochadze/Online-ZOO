9208d837985fe4af7b6112d0d31666ebed240bb1
MariamKochadze <kochadzemariam80@gmail.com>
2026-03-16 19:16:03 +0400

    feat: release version 2

--- config/configuration.ts
+++
@@ -1,7 +0,0 @@
-export interface ApplicationConfiguration {
-    readonly apiUrl: string;
-}
-
-export const configuration: ApplicationConfiguration = {
-    apiUrl: 'https://vsqsnqnxkh.execute-api.eu-central-1.amazonaws.com/prod',
-};
